8b5b2493580b2ecd8d2700b3d9dadc22cb032d13
mayaguhan <i.am.maya.guhan@gmail.com>
2022-09-08 02:39:43 -0700

    Functions Implemented
    - Generation of Mines
    - Logic to show mine upon click
    
    Pending
    
    - Function to show all mines when user clicks on a mine
    - Function to transitively reveal all adjacent cells upon click
    - Function to check the no. of mines around the tile which was clicked

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -10,7 +10,11 @@ import android.view.View;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Array to save TVs
     private ArrayList<TextView> cell_tvs;
+
+    // Array to save Mines
+    private ArrayList<int[]> mineArray;
 
     private int dpToPixel(int dp) {
         float density = Resources.getSystem().getDisplayMetrics().density;
@@ -37,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
 
         LayoutInflater li = LayoutInflater.from(this);
+
         for (int i = 0; i<10; i++) {
             for (int j=0; j<8; j++) {
                 TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
@@ -54,20 +62,67 @@ public class MainActivity extends AppCompatActivity {
                 cell_tvs.add(tv);
             }
         }
+
+        // Generate mines for tiles and save it in an ArrayList
+        generateMines();
+
+//        // Generate mines
+//        ArrayList<int[]> mineArray = generateMines();
+//
+//        for (int i=0; i<4; i++){
+//
+//            // Retrieve mine
+//
+//            System.out.println(mineArray.get(i));
+//            int[] currentMine = mineArray.get(i);
+//
+//            // Create TextView
+//            TextView tv = (TextView) li.inflate(R.layout.mine_cell_layout, grid, false);
+//            tv.setTextColor(Color.GRAY);
+//
+//            // TODO: Function will have to change to onClickMine
+//            tv.setOnClickListener(this::onClickTV);
+//
+//            GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
+//            lp.rowSpec = GridLayout.spec(currentMine[0]);
+//            lp.columnSpec = GridLayout.spec(currentMine[1]);
+//
+//            grid.addView(tv,lp);
+//
+//        }
     }
 
     public void onClickTV(View view){
+
+        // Check if mine is present. If mine is present --> mineHandler
+        // Else --> safeHandler
+
         TextView tv = (TextView) view;
         int n = findIndexOfCellTextView(tv);
         int i = n/COLUMN_COUNT;
         int j = n%COLUMN_COUNT;
-        tv.setText(String.valueOf(i)+String.valueOf(j));
-        if (tv.getCurrentTextColor() == Color.GRAY) {
-            tv.setTextColor(Color.GREEN);
-            tv.setBackgroundColor(Color.parseColor("lime"));
-        }else {
-            tv.setTextColor(Color.GRAY);
-            tv.setBackgroundColor(Color.LTGRAY);
+
+        // TODO: Insert if-else handler when mine is present vs absent
+
+        int[] mine = new int[2];
+        mine[0] = i;
+        mine[1] = j;
+
+        System.out.println("HEllo: " + mine[0] + mine[1]);
+        Boolean minePresent = this.checkIfPresent(mine);
+
+        if (minePresent){
+            this.mineHandler(tv, i, j);
+        } else {
+            tv.setText(String.valueOf(i)+String.valueOf(j));
+            if (tv.getCurrentTextColor() == Color.GRAY) {
+                tv.setTextColor(Color.GREEN);
+                tv.setBackgroundColor(Color.parseColor("lime"));
+            }else {
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.LTGRAY);
+            }
+
         }
     }
 
@@ -77,5 +132,74 @@ public class MainActivity extends AppCompatActivity {
                 return n;
         }
         return -1;
+    }
+
+
+    // Function to generate ArrayList of Mines. Mines are in int[] format.
+    private void generateMines(){
+
+        mineArray = new ArrayList<int[]>();
+
+
+        // Generate random number
+        Random rand = new Random();
+
+        for (int i=0; i<4; i++){
+
+            // Int array to store coordinates of a mine
+            int[] singleMine = new int[2];
+
+            // Row Num
+            int y = rand.nextInt(10);
+            singleMine[0] = y;
+
+
+            // Col Num
+            int x = rand.nextInt(8);
+            singleMine[1] = x;
+
+            System.out.print(y + " " + x);
+
+            // Add single mine to mines array
+            mineArray.add(singleMine);
+        }
+
+        System.out.println("HELP:" + mineArray);
+    }
+
+    // Function to check if mine is present in selected square
+    public Boolean checkIfPresent(int[] mine){
+        int i = mine[0];
+        int j = mine[1];
+
+        for (int[] presentMine : mineArray){
+            if ((i == presentMine[0]) && (j == presentMine[1])){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Function to handle when user clicks on mines
+    public void mineHandler(TextView view, int row, int col){
+
+        // TODO: Expose all mines and end game.
+        view.setText("\uD83D\uDCA3");
+        if (view.getCurrentTextColor() == Color.GRAY) {
+            view.setTextColor(Color.GREEN);
+            view.setBackgroundColor(Color.parseColor("red"));
+        }else {
+            view.setTextColor(Color.GRAY);
+            view.setBackgroundColor(Color.LTGRAY);
+        }
+
+
+
+    }
+
+    // Function to handle when user clicks on safe spot
+    public void emptyHandler(){
+        // TODO: Transitively reveal all adjacent cells
     }
 }
